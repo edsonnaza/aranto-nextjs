cfa95c1884a218ddaa58612eed2afc556c27b42e
edsonnaza <edsonnaza@gmail.com>
2024-07-29 14:21:13 -0400

    add: Fixing

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,13 @@
 import { Inter, Lusitana,Roboto } from 'next/font/google';
  
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({  weight: ['400', '700'],subsets: ['latin'] });
  
-export const lusitana = Roboto({
+export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
+
+export const roboto = Roboto({
+    weight: ['100', '300'],
+    subsets: ['latin'],
+  });
